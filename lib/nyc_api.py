5a927c9e9220b1a54b96a06f9ed9042c776ef08c
Winnie <wimacoj@gmail.com>
2024-03-09 15:57:32 +0300

    Completed assignment

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,14 +1,28 @@
-
 import requests
 import json
 
 class GetPrograms:
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        try:
+            response = requests.get(URL)
+            response.raise_for_status() 
+            return response.json() 
+        except requests.exceptions.RequestException as e:
+            print("Error fetching data:", e)
+            return None
+    
+    def program_school(self):
+        programs_list = []
+        programs = self.get_programs() 
+        if programs is not None:
+            for program in programs:
+                programs_list.append(program.get("agency", "Unknown"))  
+        return programs_list
 
-    response = requests.get(URL)
-    return response.content
+programs = GetPrograms()
+programs_schools = programs.program_school()
 
-programs = GetPrograms().get_programs()
-print(programs)
+for school in set(programs_schools):
+    print(school)
